842b83c1c526e6881a68cfee959afa21cdbb24ba
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-13 00:25:17 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,20 @@
+##################################
+####  Build Square Array Calculator
+################################
+###############################
+
+
 def square_array(array)
-  # your code here
+  i = 0 
+  while i < array.length do 
+    puts (array[i])**2
+    i += 1
+  end
 end
+
+numbers = [1, 2, 3]
+new_numbers = [9, 10, 16, 25]
+
+square_array(numbers)
+square_array(new_numbers)
+
