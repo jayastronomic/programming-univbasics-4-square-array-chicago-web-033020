a1999a388191b7e0cd62f12772bb1b3000beeccd
Julian Antonio Smith <juliansmith94@hotmail.com>
2020-03-13 00:48:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -5,23 +5,15 @@
 
 
 def square_array(array)
-  
-# Allocate an array with the same size as `numbers`
-# so that the runtime does not have to resize it from time to time
-
-result = Array.new(array.size)
-  
   i = 0 
+  
   while i < array.length do 
     puts (array[i])**2
     i += 1
   end
-  return result 
+
 end
-
-numbers = [1, 2, 3]
-new_numbers = [9, 10, 16, 25]
-
-square_array(numbers) 
-square_array(new_numbers)
+numbers = [1,2,3]
+result = []
+result << square_array(numbers)
 
